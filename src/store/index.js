0915493bdf0de360cc01cd0760f801ba2a7da65e
CodyClark07 <codyclark2010@hotmail.com>
2020-07-30 15:41:32 -0600

    own api key

--- src/store/index.js
+++ src/store/index.js
@@ -7,8 +7,8 @@ const _api = axios.create({
   baseURL: "https://trackapi.nutritionix.com/v2/",
   timeout: 3000,
   headers: {
-    "x-app-key": "2eb17763bc92fced80f94b2f64f1ee05",
-    "x-app-id": "b6e0e4a2"
+    "x-app-key": "8d5ce5f016cbab723455273590ad0d4a",
+    "x-app-id": "d1e94f3c"
   }
 });
 
